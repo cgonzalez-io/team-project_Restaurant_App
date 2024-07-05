b1717735456b53dba5f6dab06629d1799444c3bf
Restyled.io <commits@restyled.io>
2024-07-05 04:12:04 +0000

    Restyled by clang-format ${restyler.version}

--- app/src/test/java/restaurant/repository/RestaurantTableImplTest.java
+++ app/src/test/java/restaurant/repository/RestaurantTableImplTest.java
@@ -1,91 +1,92 @@
 package restaurant.repository;
 
-import org.junit.jupiter.api.*;
-import restaurant.entity.RestaurantTable;
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.sql.SQLException;
 import java.util.List;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+import restaurant.entity.RestaurantTable;
 
 public class RestaurantTableImplTest {
 
-    private RestaurantTableRepositoryImpl repository;
+  private RestaurantTableRepositoryImpl repository;
 
-    @BeforeEach
-    public void setUp() throws SQLException {
-        repository = new RestaurantTableRepositoryImpl();
+  @BeforeEach
+  public void setUp() throws SQLException {
+    repository = new RestaurantTableRepositoryImpl();
 
-        cleanUpDatabase();
+    cleanUpDatabase();
+  }
+
+  @AfterEach
+  public void tearDown() throws SQLException {
+    cleanUpDatabase();
+  }
+
+  private void cleanUpDatabase() throws SQLException {
+    List<RestaurantTable> allTables = repository.findAll();
+    for (RestaurantTable table : allTables) {
+      repository.delete(table.getTableID());
     }
+  }
 
-    @AfterEach
-    public void tearDown() throws SQLException {
-        cleanUpDatabase();
-    }
+  @Test
+  public void test_Insert() throws SQLException {
+    RestaurantTable table = new RestaurantTable(1, 2, 4);
 
-    private void cleanUpDatabase() throws SQLException {
-        List<RestaurantTable> allTables = repository.findAll();
-        for(RestaurantTable table : allTables) {
-            repository.delete(table.getTableID());
-        }
-    }
+    repository.insert(table);
 
-    @Test
-    public void test_Insert() throws SQLException {
-        RestaurantTable table = new RestaurantTable(1, 2, 4);
+    RestaurantTable retrievedTable = repository.findById(1);
 
-        repository.insert(table);
+    assertNotNull(retrievedTable, "Expected table to be retrieved");
+    assertEquals(table.getTableID(), retrievedTable.getTableID());
+  }
 
-        RestaurantTable retrievedTable = repository.findById(1);
+  @Test
+  public void test_FindById_NotFound() throws SQLException {
+    RestaurantTable retrievedTable = repository.findById(999);
 
-        assertNotNull(retrievedTable, "Expected table to be retrieved");
-        assertEquals(table.getTableID(), retrievedTable.getTableID());
-    }
+    assertNull(retrievedTable, "Expected no table to be retrieved");
+  }
 
-    @Test
-    public void test_FindById_NotFound() throws SQLException {
-        RestaurantTable retrievedTable = repository.findById(999);
+  @Test
+  public void test_Delete() throws SQLException {
+    RestaurantTable table = new RestaurantTable(2, 2, 4);
+    repository.insert(table);
 
-        assertNull(retrievedTable, "Expected no table to be retrieved");
-    }
+    repository.delete(2);
 
-    @Test
-    public void test_Delete() throws SQLException {
-        RestaurantTable table = new RestaurantTable(2, 2, 4);
-        repository.insert(table);
+    RestaurantTable deletedTable = repository.findById(2);
 
-        repository.delete(2);
+    assertNull(deletedTable, "Expected table to be deleted");
+  }
 
-        RestaurantTable deletedTable = repository.findById(2);
+  @Test
+  public void test_Update() throws SQLException {
+    RestaurantTable table = new RestaurantTable(3, 3, 4);
+    repository.insert(table);
 
-        assertNull(deletedTable, "Expected table to be deleted");
-    }
+    table.setTableNumber(4);
 
-    @Test
-    public void test_Update() throws SQLException {
-        RestaurantTable table = new RestaurantTable(3, 3, 4);
-        repository.insert(table);
+    repository.update(table);
 
-        table.setTableNumber(4);
+    RestaurantTable updatedTable = repository.findById(3);
 
-        repository.update(table);
+    assertNotNull(updatedTable, "Expected updated table to be retrieved");
+    assertEquals(table.getTableID(), updatedTable.getTableID());
+    assertEquals(4, updatedTable.getTableNumber());
+  }
 
-        RestaurantTable updatedTable = repository.findById(3);
+  @Test
+  public void test_FindAll() throws SQLException {
+    RestaurantTable table1 = new RestaurantTable(4, 4, 4);
+    RestaurantTable table2 = new RestaurantTable(5, 5, 5);
+    repository.insert(table1);
+    repository.insert(table2);
 
-        assertNotNull(updatedTable, "Expected updated table to be retrieved");
-        assertEquals(table.getTableID(), updatedTable.getTableID());
-        assertEquals(4, updatedTable.getTableNumber());
-    }
+    List<RestaurantTable> allTables = repository.findAll();
 
-    @Test
-    public void test_FindAll() throws SQLException {
-        RestaurantTable table1 = new RestaurantTable(4, 4, 4);
-        RestaurantTable table2 = new RestaurantTable(5, 5, 5);
-        repository.insert(table1);
-        repository.insert(table2);
-
-        List<RestaurantTable> allTables = repository.findAll();
-
-        assertNotNull(allTables, "Expected list of tables to be retrieved");
-        assertEquals(2, allTables.size(), "Expected exactly two tables");
-    }
+    assertNotNull(allTables, "Expected list of tables to be retrieved");
+    assertEquals(2, allTables.size(), "Expected exactly two tables");
+  }
 }
